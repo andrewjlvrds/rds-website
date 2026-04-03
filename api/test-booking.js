e8bb6884900b0f9118bfa49af1c29dbc7c2c46b3
andrewjlvrds <andrew@ridedownsouth.com>
2026-04-03 09:13:27 +0200

    Add test endpoint to debug routing

---
+++ api/test-booking.js
@@ -0,0 +1,15 @@
+module.exports = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return res.status(200).json({
+    ok: true,
+    method: req.method,
+    body: req.body ? 'received' : 'empty'
+  });
+};
