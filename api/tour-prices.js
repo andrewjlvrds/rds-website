729c21f987b9178d347b3cbdec7bd8284c2f5913
andrewjlvrds <andrew@ridedownsouth.com>
2026-04-11 13:11:54 +0200

    fix: open CORS on tour-prices to allow www and other origins

--- api/tour-prices.js
+++ api/tour-prices.js
@@ -9,7 +9,7 @@ var cache = {
 };
 
 module.exports = function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ridedownsouth.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
@@ -119,3 +119,4 @@ module.exports = function handler(req, res) {
       });
     });
 };
+
